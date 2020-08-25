2159e9633ab43e3c8db4a2176a8ddd75d7b3543b
Hadi Ahmed <hady.ashour@ibnsina-pharma.com>
2020-08-25 10:12:47 +0200

    - Updating AndroidManifest.xml and styles.xml - Updating vcs.xml so it work inside the project path directly

--- Apps/Android/Sahla/app/src/main/java/com/sahla/sahla/utils/Constants.kt
+++ Apps/Android/Sahla/app/src/main/java/com/sahla/sahla/utils/Constants.kt
@@ -1,7 +1,10 @@
 package com.sahla.sahla.utils
 
 object Constants {
+    // Constants for preference Keys
     const val SAHLA_PREFERENCE = "sahla_database_key"
     const val SELECTED_LANGUAGE_KEY = "selected_language_key"
+
+    // API Base Url
     const val API_BASE_URL = "sahla_database_key"
 }
